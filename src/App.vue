6386fc7ae06ad1bc725f0753ccecc76a81ee6ade
Adrix2238 <2238adrix@gmail.com>
2020-01-04 13:30:58 +0100

    Finissimo
    Santi top

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,65 @@
 <template>
   <div id="app">
     <div class="agraiments">
-      <div class="aniol">
-        <img src="./assets/aniol.png" class="aniolvideo">
-        <h2 class="aniolmaeso">Aniol Maeso</h2>
-        <h2 class="aniolmaeso2">Videographer</h2> 
+      <div class="flex-container">
+        <div class="aniol">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="aniolnom">Aniol Maeso</h2>
+          <h2 class="aniolrol">Videographer</h2> 
+        </div>
+
+        <div class="adria">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="adrianom">Adrià Crehuet</h2>
+          <h2 class="adriarol">Programmer</h2> 
+        </div>
       </div>
 
-      <div class="adria">
-        <img src="./assets/aniol.png" class="adriaprogramer">
-        <h2 class="adria1">Adrià Crehuet</h2>
-        <h2 class="adria2">Programmer</h2> 
+      <div class="flex-container">
+        <div class="aniol">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="aniolnom">Aniol Maeso</h2>
+          <h2 class="aniolrol">Videographer</h2> 
+        </div>
+
+        <div class="adria">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="adrianom">Adrià Crehuet</h2>
+          <h2 class="adriarol">Programmer</h2> 
+        </div>
       </div>
 
-      <div class="aniol2">
-        <img src="./assets/aniol.png" class="aniolvideo">
-        <h2 class="aniolmaeso">Aniol Maeso</h2>
-        <h2 class="aniolmaeso2">Videographer</h2> 
+       <div class="flex-container">
+        <div class="aniol">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="aniolnom">Aniol Maeso</h2>
+          <h2 class="aniolrol">Videographer</h2> 
+        </div>
+
+        <div class="adria">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="adrianom">Adrià Crehuet</h2>
+          <h2 class="adriarol">Programmer</h2> 
+        </div>
       </div>
 
-      <div class="adria22">
-        <img src="./assets/aniol.png" class="adriaprogramer">
-        <h2 class="adria1">Adrià Crehuet</h2>
-        <h2 class="adria2">Programmer</h2> 
+      <div class="flex-container" >
+        <div class="aniol">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="aniolnom">Aniol Maeso</h2>
+          <h2 class="aniolrol">Videographer</h2> 
+        </div>
+
+        <div class="adria">
+          <img src="./assets/aniol.png" class="foto">
+          <h2 class="adrianom">Adrià Crehuet</h2>
+          <h2 class="adriarol">Programmer</h2> 
+        </div>
+
       </div>
 
-      
-        <h1 class="ayuda">Flexbox</h1>
+   
+        <h1 class="ayuda">Agradecimientos</h1>
           <ul class="container flex">
             <li class="item flex-item">
               <p class="p1">Enric Crehuet</p>
@@ -110,17 +143,19 @@ export default {
   -webkit-flex-flow: row wrap;
   justify-content:center;
   z-index: auto;
-  margin-top: 76rem; 
+  margin-top: 2rem; 
   margin-bottom: 10rem;
 }
 
-.ayuda {
-  margin-top: 70rem;
-  z-index:auto;
+h1 {
+  margin-top: 5rem;
+  text-align: left;
+  margin-left:11.5rem;
 }
 
+
 .p1 {
-  margin-right:4rem;
+  margin-right:3rem;
   font-size: 1.26rem;
 }
 
@@ -132,19 +167,19 @@ export default {
 
 
 .p1{
-        -webkit-animation: color-change 15s infinite;
-        -moz-animation: color-change 16s infinite;
-        -o-animation: color-change 15s infinite;
-        -ms-animation: color-change 16s infinite;
-        animation: color-change 15s infinite;
+        -webkit-animation: color-change 20s infinite;
+        -moz-animation: color-change 22s infinite;
+        -o-animation: color-change 20s infinite;
+        -ms-animation: color-change 22s infinite;
+        animation: color-change 20s infinite;
     }
 
 .p2{
-        -webkit-animation: color-change 10s infinite;
-        -moz-animation: color-change 11s infinite;
-        -o-animation: color-change 10s infinite;
-        -ms-animation: color-change 11s infinite;
-        animation: color-change 10s infinite;
+        -webkit-animation: color-change 13s infinite;
+        -moz-animation: color-change 14s infinite;
+        -o-animation: color-change 13s infinite;
+        -ms-animation: color-change 14s infinite;
+        animation: color-change 13s infinite;
     }
 
     @-webkit-keyframes color-change {
@@ -173,84 +208,46 @@ export default {
         100% { color:black; }
     }
 
-
-
-
-.aniol2 {
-  float:left;
-  margin-left:-5rem;
-  margin-top: 15rem;
-}
-
-.adria22 {
-  float:right;
-  margin-right:25rem;
-  margin-top: 18rem;
-}
-
-
-
-
-.adria {
-  float:right;
-  margin-right:25rem;
-  margin-top: 15rem;
-}
-
-.adriaprogramer{
-  position: absolute;
-  filter:blur(0.1rem);
-  z-index: -10px;
-}
-
-.adria1 {
-  position: relative;
-  margin-top: -2.2rem;
-  margin-right: -12rem; 
-  font-size: 2.93rem;
-  z-index: 12px;
-
-}
-
-.adria2 { 
-  position: relative;
-  margin-top: 11.5rem;
-  margin-right: -31rem;
-  font-size: 1.75rem;
-  z-index: 12px;
-  transform: rotate(90deg);
+.flex-container {
+  display: flex;
+  flex-direction: row;
+  justify-content:space-around;
+  margin-top:8rem;
 }
 
 
 .aniol {
-  float:left;
-  margin-left:-5rem;
-  margin-top: 8rem;
+  margin-left: 2rem;
 }
-
-.aniolvideo {
-  position: absolute;
-  filter:blur(0.1rem);
-  z-index: -1px;
-}
-
-.aniolmaeso {
-  position: relative;
-  margin-top: -2.2rem;
-  margin-left: 19rem; 
+.aniolnom {
+  margin-top: -21rem;
   font-size: 2.93rem;
-  z-index: 11px;
+  margin-left: 3.5rem;
 }
-
-.aniolmaeso2 { 
-  position: relative;
+.aniolrol {
   margin-top: 11.5rem;
-  margin-left: -0.5rem;
+  margin-left: -16.2rem;
   font-size: 1.75rem;
-  z-index: 10px;
   transform: rotate(-90deg);
-
 }
+
+.adria {
+  margin-top: 10rem;
+  margin-right: 2rem;
+}
+
+.adrianom {
+  margin-top: -21rem;
+  font-size: 2.93rem;
+  margin-right: 3.5rem;
+}
+.adriarol {
+  margin-top: 11.5rem;
+  margin-right: -16rem;
+  font-size: 1.75rem;
+  transform: rotate(90deg);
+}
+
 html,
 body {
   margin: 0;
@@ -272,164 +269,9 @@ body * {
   transition: filter 2s ease-in-out;
 }
 
-h1 {
-  font-family: "Tiempos Headline Regular", Times, serif;
-  font-size: 3.15rem;
-  font-weight: 500;
-  text-align: left;
-  margin-top: 5.1rem;
-  margin-left: 13.5rem;
-  color: #f5f9fb;
+h2 {
+  filter:blur(0.03rem);
 }
-
-@media only screen and (max-width: 450px) and (min-width: 410px) {
-
-.aniol {
-  float:left;
-  margin-left:-8rem;
-  margin-top: 8rem; 
-  
-}
-.aniolvideo {
-  position: absolute;
-  filter:blur(0.1rem);
-  z-index: -1px;
-}
-.aniolmaeso {
-  position: relative;
-  margin-top: -1.5rem;
-  margin-left: 19rem; 
-  font-size: 2.2rem;
-  z-index: 11px;
-  filter:blur(0.07rem);
-  
-}
-.aniolmaeso2 { 
-  position: relative;
-  margin-top: 11.5rem;
-  margin-left: -0.5rem;
-  font-size: 1.75rem;
-  z-index: 10px;
-  transform: rotate(-90deg);
-}
-
-.adria {
-  float:right;
-  margin-right:22rem;
-  margin-top: 10rem;
-}
-
-.adriaprogramer{
-  position: absolute;
-  filter:blur(0.1rem);
-  z-index: -10px;
-}
-.adria1 {
-  position: relative;
-  margin-top: -1.6rem;
-  margin-right: -12rem; 
-  font-size: 2.2rem;
-  z-index: 12px;
-  filter:blur(0.07rem);
-}
- 
-.aniol2 {
-  float:left;
-  margin-left:-8rem;
-  margin-top: 10rem;
-}
-
-.adria22 {
-  float:right;
-  margin-right:22rem;
-  margin-top: 10rem;
-}
-
-.adria2 { 
-  position: relative;
-  margin-top: 11.5rem;
-  margin-right: -31rem;
-  font-size: 1.75rem;
-  z-index: 12px;
-  transform: rotate(90deg);
-}
-}
-
-
-@media screen and (max-width: 399px) and (min-width: 340px) {
-
-.aniol {
-  float:left;
-  margin-left:-10.1rem;
-  margin-top: 8rem; 
-  
-}
-.aniolvideo {
-  position: absolute;
-  filter:blur(0.1rem);
-  z-index: -1px;
-}
-.aniolmaeso {
-  position: relative;
-  margin-top: -1.5rem;
-  margin-left: 19rem; 
-  font-size: 2.2rem;
-  z-index: 11px;
-  filter:blur(0.07rem);
-  
-}
-.aniolmaeso2 { 
-  position: relative;
-  margin-top: 11.5rem;
-  margin-left: -0.5rem;
-  font-size: 1.75rem;
-  z-index: 10px;
-  transform: rotate(-90deg);
-}
-
-.adria {
-  float:right;
-  margin-right:19.7rem;
-  margin-top: 8.5rem;
-}
-
-.adriaprogramer{
-  position: absolute;
-  filter:blur(0.1rem);
-  z-index: -10px;
-}
-.adria1 {
-  position: relative;
-  margin-top: -1.6rem;
-  margin-right: -12rem; 
-  font-size: 2.2rem;
-  z-index: 12px;
-  filter:blur(0.07rem);
-}
- 
-.aniol2 {
-  float:left;
-  margin-left:-10.1rem;
-  margin-top: 8.5rem;
-}
-
-.adria22 {
-  float:right;
-  margin-right:19.7rem;
-  margin-top: 8.5rem;
-}
-
-.adria2 { 
-  position: relative;
-  margin-top: 11.5rem;
-  margin-right: -31rem;
-  font-size: 1.75rem;
-  z-index: 12px;
-  transform: rotate(90deg);
-}
-
-}
-
 
 </style>
 
