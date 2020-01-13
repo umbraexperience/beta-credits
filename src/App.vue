7aa66e007556a2e85ecaab6f1fe7c999874f0400
Adrix2238 <2238adrix@gmail.com>
2020-01-13 17:51:42 +0100

    95%

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <meta name="viewport" content="width=device-width, user-scalable=no">
     <div class="agraiments">
       <div class="flex-container">
         <div v-for="person in persons" :key="person.id" class="person">
@@ -10,117 +11,11 @@
       </div>
 
     <div class="crowfunding">
-    <h1 class="ayuda">Agradecimientos</h1>
+    <h1 class="gracies">Agradecimientos </h1>
         <div class="wrapper">
           <div v-for="mecena in mecenes" :key="mecena.id" class="paypal">{{ mecena.name }}</div>
         </div>
     </div>
-
-      <!--  <div class="flex-container">
-        <div class="aniol">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="aniolnom">Aniol Maeso</h2>
-          <h2 class="aniolrol">Videographer</h2>
-        </div>
-
-        <div class="adria">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="adrianom">Adrià Crehuet</h2>
-          <h2 class="adriarol">Programmer</h2>
-        </div>
-      </div>
-
-      <div class="flex-container">
-        <div class="aniol">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="aniolnom">Aniol Maeso</h2>
-          <h2 class="aniolrol">Videographer</h2>
-        </div>
-
-        <div class="adria">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="adrianom">Adrià Crehuet</h2>
-          <h2 class="adriarol">Programmer</h2>
-        </div>
-      </div>
-
-      <div class="flex-container">
-        <div class="aniol">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="aniolnom">Aniol Maeso</h2>
-          <h2 class="aniolrol">Videographer</h2>
-        </div>
-
-        <div class="adria">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="adrianom">Adrià Crehuet</h2>
-          <h2 class="adriarol">Programmer</h2>
-        </div>
-      </div>
-
-      <div class="flex-container">
-        <div class="aniol last">
-          <img src="./assets/aniol.png" class="foto" />
-          <h2 class="aniolnom">Aniol Maeso</h2>
-          <h2 class="aniolrol">Videographer</h2>
-        </div>
-      </div> 
-
-      <h1 class="ayuda">Agradecimientos</h1>
-      <div class="gracies">
-        <ul class="container flex">
-          <li class="item flex-item">
-            <p class="p1">Enric Crehuet</p>
-            <p class="p2">Adrià Solà</p>
-            <p class="p1">Mayra Segura</p>
-            <p class="p2">Tania Tarrés</p>
-            <p class="p1">Roser Juanola</p>
-            <p class="p2">persona 1</p>
-            <p class="p1">persona 2</p>
-            <p class="p2">persona 3</p>
-          </li>
-          <li class="item flex-item">
-            <p class="p1">Alfonso Ortega</p>
-            <p class="p2">Carles Ponce</p>
-            <p class="p1">Xixi Chen</p>
-            <p class="p2">Èric Bautista</p>
-            <p class="p1">Pilar Calatayud</p>
-            <p class="p2">persona 1</p>
-            <p class="p1">persona 2</p>
-            <p class="p2">persona 3</p>
-          </li>
-          <li class="item flex-item">
-            <p class="p1">Iván Cortés</p>
-            <p class="p2">Ferran Crehuet</p>
-            <p class="p1">David Monje</p>
-            <p class="p2">Tania Muñoz</p>
-            <p class="p1">persona 1</p>
-            <p class="p2">persona 2</p>
-            <p class="p1">persona 3</p>
-            <p class="p2">persona 4</p>
-          </li>
-          <li class="item flex-item">
-            <p class="p1">Santi Ramió</p>
-            <p class="p2">Roser Canals</p>
-            <p class="p1">Roser Carbonell</p>
-            <p class="p2">Marc Rovira</p>
-            <p class="p1">persona 1</p>
-            <p class="p2">persona 2</p>
-            <p class="p1">persona 3</p>
-            <p class="p2">persona 4</p>
-          </li>
-          <li class="item flex-item">
-            <p class="p1">Aina Alsina</p>
-            <p class="p2">Laura Amargant</p>
-            <p class="p1">Encarna Jiménez</p>
-            <p class="p2">Marta Germana</p>
-            <p class="p1">persona 1</p>
-            <p class="p2">persona 2</p>
-            <p class="p1">persona 3</p>
-            <p class="p2">persona 4</p>
-          </li>
-        </ul> 
-      </div> -->
     </div>
   </div>
 </template>
@@ -132,34 +27,40 @@ export default {
   data: function() {
     return {
       persons: [
+        { id: 0, name: "Marc Vila", role: "Developer", img: "" },
+        { id: 0, name: "Santi Cros", role: "Developer", img: "" },
         { id: 0, name: "Abel Martínez", role: "Developer", img: "" },
         { id: 0, name: "Adrià Crehuet", role: "Developer", img: "" },
         { id: 0, name: "Afra Ramió", role: "Developer", img: "" },
         { id: 0, name: "Aniol Maeso", role: "Developer", img: "" },
-        { id: 0, name: "Marta Gallego", role: "Developer", img: "" },
-        { id: 0, name: "Marc Vila", role: "Developer", img: "" },
-        { id: 0, name: "Santi Cros", role: "Developer", img: "" }
+        { id: 0, name: "Marta Gallego", role: "Developer", img: "" }
       ],
 
       mecenes: [
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"},
-        { id: 0, name: "Abel Martínez"}
+        { id: 0, name: "Enric Crehuet"},
+        { id: 0, name: "Alfonso Ortega"},
+        { id: 0, name: "Iván Cortés"},
+        { id: 0, name: "Santi Ramió"},
+        { id: 0, name: "Mar Palma"},
+        { id: 0, name: "Aina Alsina"},
+        { id: 0, name: "Adrià Solà"},
+        { id: 0, name: "Carles Ponce"},
+        { id: 0, name: "Ferran Crehuet"},
+        { id: 0, name: "Roser Canals"},
+        { id: 0, name: "Laura Amargant"},
+        { id: 0, name: "Mayra Segura"},
+        { id: 0, name: "Xixi Chen"},
+        { id: 0, name: "David Monje"},
+        { id: 0, name: "Roser Carbonell"},
+        { id: 0, name: "Encarna Jiménez"},
+        { id: 0, name: "Tania Tarrés"},
+        { id: 0, name: "Èric Bautista"},
+        { id: 0, name: "Tania Muñoz"},
+        { id: 0, name: "Marc Rovira"},
+        { id: 0, name: "Marta Germana"},
+        { id: 0, name: "Roser Juanola"},
+        { id: 0, name: "Pilar Calatayud"},
+        { id: 0, name: "Enrique Cros"}
       ],
     };
   }
@@ -198,14 +99,17 @@ export default {
 .font-light {
   font-family: "Tiempos Headline", Times, serif;
   font-weight: 300;
+  filter: blur(0.07rem);
 }
 .font-regular {
   font-family: "Tiempos Headline", Times, serif;
   font-weight: 400;
+  filter: blur(0.07rem);
 }
 .font-medium {
   font-family: "Tiempos Headline", Times, serif;
   font-weight: 500;
+  filter: blur(0.07rem);
 }
 .flex {
   padding: 0;
@@ -232,23 +136,32 @@ export default {
 
 .wrapper {
   display: grid;
-  grid-gap: 10px;
+  grid-gap: 2rem 1rem;
   grid-template-columns: repeat(5, 1fr);
-  width:90%;
   justify-content: space-evenly;
-
-
-  border: 2px solid yellow;
+  width: 70%;
+  margin: 0 auto;
+  margin-top: 3rem;
+  margin-bottom: 5rem;
 }
 
 .paypal {
-  border: 2px solid red;
-  
+  font-family: "Tiempos Headline", Times, serif;
+  font-weight: 500;
+  filter: blur(0.04rem);
 
+  
 }
 
+.gracies {
+  filter: blur(0.07rem);
+  margin-top: 10rem;
+  width: auto
+  
+}
+
+
 .last {
-  border: 2px solid red;
   align-self: left;
   justify-items: left;
 }
@@ -260,15 +173,15 @@ h1 {
 }
 
 
-.p1 {
+.paypal:nth-child(odd) {
   -webkit-animation: color-change 14s infinite;
   -moz-animation: color-change 14s infinite;
   -o-animation: color-change 14s infinite;
   -ms-animation: color-change 14s infinite;
   animation: color-change 14s infinite;
-}
+} 
 
-.p2 {
+.paypal:nth-child(even)  {
   -webkit-animation: color-change 14s infinite;
   -moz-animation: color-change 14s infinite;
   -o-animation: color-change 14s infinite;
@@ -377,27 +290,9 @@ h1 {
 }
 
 .person:last-child {
-  width: 0 auto;
-}
-
-@media screen and (min-width: 640px) {
-  .person {
-    width: 50%;
-  }
-  .person:nth-child(even) {
-    padding-top: 8rem;
-  }
-}
-
-@media screen and (min-width: 1000px) {
-  .person .name {
-    font-size: 2.93rem;
-    margin-top: -21.5rem;
-  }
-  .person .role {
-    margin-top: 10rem;
-    font-size: 2.2rem;
-  }
+  width: auto;
+  border: 1px solid red; 
+  justify-content: left;
 }
 
 .aniolrol {
@@ -429,6 +324,8 @@ body {
   margin: 0;
   padding: 0;
   height: 100%;
+
+
 }
 body {
   background-color: black;
@@ -449,67 +346,47 @@ h2 {
   filter: blur(0.03rem);
 }
 
-@media screen and (max-width: 640px) {
-  /*   * {
-    border: 2px solid red;
-  } */
-  html,
-  body {
-    overflow-x: hidden;
-  }
 
-  .aniolnom {
-    font-size: 2.2rem;
-    filter: blur(0.07rem);
+@media screen and (min-width: 640px) {
+  .person {
+    width: 50%;
   }
-
-  .aniolrol {
-    font-size: 1.75rem;
+  .person:nth-child(even) {
+    padding-top: 8rem;
   }
-
-  .adrianom {
-    font-size: 2.2rem;
-    filter: blur(0.07rem);
-  }
-
-  .adriarol {
-    font-size: 1.75rem;
-  }
-
-  .adria {
-    margin-left: 5rem;
-  }
-
-  .aniol {
-    margin-right: 5rem;
-  }
-
-  .flex-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 10rem;
-    margin-bottom: 0rem;
-  }
-
-  .flex {
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    flex-direction: row;
-  }
-  h1 {
-    margin-top: 8rem;
-    text-align: center;
-    margin-left: 0rem;
-    margin-right: 0rem;
-    font-size: 1.9rem;
-  }
+ 
 }
 
-.p1 .p2 {
-  font-size: 1.15rem;
-  text-align: center;
+@media screen and (min-width: 1000px) {
+  .person .name {
+    font-size: 2.93rem;
+    margin-top: -21.5rem;
+  }
+  .person .role {
+    margin-top: 10rem;
+    font-size: 2.2rem;
+  }
+  
+}
+
+
+@media screen and (max-width: 640px) {
+  .paypal {
+   text-align: center;
+   font-size: 1rem;
+   border: 1px solid red;
+   
+  }
+  .gracies {
+    margin-top: 7rem;
+    margin-left: 0;
+    text-align: center;
+  }
+  .wrapper {
+    grid-template-columns: repeat(3, 1fr);
+    grid-gap: 2rem 2rem;
+  }
+ 
 }
 
 
